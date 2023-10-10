2a77144879ac45f10780fe72dfc16ab17b196ce0
rio1412 <118584074+rio1412@users.noreply.github.com>
2023-10-10 20:06:08 +0900

    app.py

--- app.py
+++ app.py
@@ -1,5 +1,20 @@
 import streamlit as st
 from convert import process_text
+import subprocess
+
+# spaCyモデルの存在を確認し、なければダウンロードする関数
+def download_spacy_model():
+    try:
+        # spaCyのja_core_news_smモデルが存在しない場合、ダウンロードする
+        subprocess.run(['python', '-m', 'spacy', 'download', 'ja_core_news_sm'])
+        return 'spaCy Model Downloaded Successfully!'
+    except Exception as e:
+        return f'Error: {str(e)}'
+
+# spaCyモデルの存在を確認し、なければダウンロードを試みる
+result = download_spacy_model()
+st.write(result)
+
 
 # Load the HTML template
 html_template = open("index.html", "r", encoding="utf-8").read()
